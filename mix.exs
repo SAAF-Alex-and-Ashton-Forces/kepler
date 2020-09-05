94dad4f63953192c6e4b186176ce6e1c0739e317
Ashton Wiersdorf <ashton.wiersdorf@tazworks.com>
2020-09-04 19:46:00 -0600

    Add pow_assent deps

--- mix.exs
+++ mix.exs
@@ -47,7 +47,10 @@ defmodule Kepler.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:pow, "~> 1.0.20"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:pow_assent, "~> 0.4.8"},
+      {:certifi, "~> 2.4"},
+      {:ssl_verify_fun, "~> 1.1"}
     ]
   end
 
