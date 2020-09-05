eba26bbe3feceb9ac8887937b4fc901dc26ec1d5
Ashton Wiersdorf <ashton.wiersdorf@tazworks.com>
2020-09-04 18:43:19 -0600

    Add docker.db task for dev

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :kepler, Kepler.Repo,
   password: "postgres",
   database: "kepler_dev",
   hostname: "localhost",
+  port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
