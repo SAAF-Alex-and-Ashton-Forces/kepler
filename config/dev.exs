d5df023f5a600b1e7986f5c1fdf17689f892bd01
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-10-15 10:51:24 -0600

    Add Tailwind, login page

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :kepler, KeplerWeb.Endpoint,
   secret_key_base: "fjgDjV3Gct9favURu+cWqnCIrZc4jjiUf0sGsrotWxOvSO+IodEMVTpnpAqv8tt5",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
